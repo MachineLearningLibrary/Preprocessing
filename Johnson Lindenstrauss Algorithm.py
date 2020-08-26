84c607aa65a8c45101b1258b7e041025bcced14e
MachineLearningLibrary <69740045+MachineLearningLibrary@users.noreply.github.com>
2020-08-26 15:16:16 +1000

    Add files via upload

--- Johnson Lindenstrauss Algorithm.py
+++ Johnson Lindenstrauss Algorithm.py
@@ -11,14 +11,14 @@ import numpy
 
 numbers = [-1, 1]
 
-X = [[1, 2, 3, 5, 8], [1, 3, 5, 7, 9], [1, 2, 3, 6, 8]]
+X = [[1, 2, 3], [1, 3, 5], [1, 2, 3], [1, 2, 5], [1, 2, 4], [1, 2, 3], [1, 2, 3], [1, 2, 4], [2, 2, 3], [1, 2, 3], [2, 3, 3], [2, 3, 5], [2, 3, 6], [2, 3, 8], [2, 5, 3], [6, 3, 3], [2, 3, 8], [2, 3, 1], [2, 3, 2], [2, 3, 3], [2, 3, 5], [3, 3, 1], [2, 3, 5]]
 
 B = 0.01
 e = 0.95
-n = 5
-d = 3
+n = 3
+d = len(X)
 
-k = int(math.log(n) * (4 + 2 * B) / (e**2 / 2 - e**3 / 3))
+k = int(math.log(n, 10) * (4 + 2 * B) / (e**2 / 2 - e**3 / 3))
 
 R = []
 for i in range(k):
@@ -26,7 +26,7 @@ for i in range(k):
     for j in range(d):
         temp.append(random.choice(numbers))
     R.append(temp)
-    
+
 matrix_X = numpy.array(X)
 matrix_R = numpy.array(R)
 
@@ -38,7 +38,7 @@ for i in range(len(Y)):
     for j in range(len(Y[i])):
         temp.append(Y[i][j] / (k)**(1/2))
     Y_result.append(temp)
-    
+
 print(Y_result)
         
 
